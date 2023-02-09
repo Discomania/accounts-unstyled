7d98976b56a72df48f376e8cbc52be2f4227acdc
Discomania <daniele.ponzo@libero.it>
2023-02-09 21:39:32 +0100

    updated versions

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: 'Semantic UI Components for Useraccounts-React',
-  version: '1.0.0',
+  version: '1.0.1',
   name: 'meteoreact:accounts-unstyled',
   git: 'https://github.com/royGil/useraccounts-react.git'
 })
 
 Package.onUse(api => {
-  api.versionsFrom('1.6.1')
+  api.versionsFrom('2.9.0')
 
   api.use('ecmascript')
   api.mainModule('index.js', ['client', 'server'])
